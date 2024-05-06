ea0857dd0af3b881ccbf6dc92dfc4c8c7ffeb6dd
fanta <tobiasmuellner@outlook.com>
2024-05-06 03:01:03 +0200

    check zeugs

--- src/main/java/de/fanta/tedisync/teamspeak/LuckPermsListener.java
+++ src/main/java/de/fanta/tedisync/teamspeak/LuckPermsListener.java
@@ -1,5 +1,6 @@
 package de.fanta.tedisync.teamspeak;
 
+import com.github.theholywaffle.teamspeak3.api.exception.TS3CommandFailedException;
 import com.github.theholywaffle.teamspeak3.api.wrapper.ClientInfo;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.event.user.UserDataRecalculateEvent;
@@ -14,11 +15,16 @@ public record LuckPermsListener(TeamSpeakBot teamSpeakBot) {
             try {
                 Collection<TeamSpeakUserInfo> teamSpeakUserInfos = teamSpeakBot.getDatabase().getUsersByUUIS(event.getUser().getUniqueId());
                 teamSpeakUserInfos.forEach(teamSpeakUserInfo -> {
-                    ClientInfo clientInfo = teamSpeakBot.getAsyncApi().getClientByUId(teamSpeakUserInfo.tsID()).getUninterruptibly();
-                    if (clientInfo != null) {
-                        if (teamSpeakBot.getAsyncApi().isClientOnline(clientInfo.getId()).getUninterruptibly()) {
-                            teamSpeakBot.updateTeamSpeakGroup(event.getUser().getUniqueId(), clientInfo);
+                    ClientInfo clientInfo = null;
+                    if (teamSpeakBot.getAsyncApi().isClientOnline(teamSpeakUserInfo.tsID()).getUninterruptibly()) {
+                        try {
+                            clientInfo = teamSpeakBot.getAsyncApi().getClientByUId(teamSpeakUserInfo.tsID()).getUninterruptibly();
+                        } catch (TS3CommandFailedException ignored) {
                         }
+                    }
+
+                    if (clientInfo != null) {
+                        teamSpeakBot.updateTeamSpeakGroup(event.getUser().getUniqueId(), clientInfo);
                     }
 
                 });
